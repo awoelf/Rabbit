97e8f7ceb1c83675fe159cd18c51f08af141c5b6
rogseo <rogseo@gmail.com>
2023-01-01 22:53:46 -0600

    Add ApolloProvider and navigator

--- client/App.js
+++ client/App.js
@@ -1,15 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
 import { StyleSheet, Text, View } from 'react-native';
 
 import { Provider } from 'react-redux';
 import store from './src/utils/store';
 import Home from './src/screens/Home';
+import Login from './src/screens/LogIn';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+
+
+const Stack = createNativeStackNavigator();
+
+const authLink = setContext((_, { headers }) => {
+  const token =  AsyncStorage.getItem('id_token');
+  console.log(token);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <Home/>
-    </Provider>
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Login">
+            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="Login" component={Login} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </Provider>
+    </ApolloProvider>
   );
 }
 
