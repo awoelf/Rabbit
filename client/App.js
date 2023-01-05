fdfaf7f0fc70b97b891b854d1815db4793e6703b
rogseo <rogseo@gmail.com>
2023-01-05 13:12:15 -0600

    add navigator

--- client/App.js
+++ client/App.js
@@ -7,29 +7,29 @@ import {
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-
-
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+
+//Navigators
+import Main from "./src/navigation/Main"
+
+// Screens
+import Header from "./Shared/Header";
+
+//Redux
 import { Provider } from 'react-redux';
 import store from './src/utils/store';
-import Home from './src/screens/Home';
-import Login from './src/screens/LogIn';
+
 
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: 'https://rabbit-app.herokuapp.com/graphql',
 });
 
 
-
-const Stack = createNativeStackNavigator();
-
 const authLink = setContext((_, { headers }) => {
   const token =  AsyncStorage.getItem('id_token');
-  console.log(token);
   return {
     headers: {
       ...headers,
@@ -49,21 +49,10 @@ export default function App() {
     <ApolloProvider client={client}>
       <Provider store={store}>
         <NavigationContainer>
-          <Stack.Navigator initialRouteName="Login">
-            <Stack.Screen name="Home" component={Home} />
-            <Stack.Screen name="Login" component={Login} />
-          </Stack.Navigator>
+          <Header/>
+          <Main/>
         </NavigationContainer>
       </Provider>
     </ApolloProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
