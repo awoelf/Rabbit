23c97cee69754e90d635ba6c6743d35c7a5a4077
awoelf <98348782+awoelf@users.noreply.github.com>
2023-01-10 14:30:12 -0600

    added message navigator

---
+++ src/navigation/MessageNavigator.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+import * as ExpoClipboard from 'expo-clipboard';
+import * as ExpoDocumentPicker from 'expo-document-picker';
+import * as ExpoFS from 'expo-file-system';
+import * as ExpoImagePicker from 'expo-image-picker';
+import * as ExpoMediaLibrary from 'expo-media-library';
+import * as ExpoNotifications from 'expo-notifications';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import {
+    createExpoClipboardService,
+    createExpoFileService,
+    createExpoNotificationService,
+    SendbirdUIKitContainer,
+    useSendbirdChat,
+} from '@sendbird/uikit-react-native';
+import SendbirdChat from '@sendbird/chat';
+
+// import screens here
+
+const NotificationService = createExpoNotificationService(ExpoNotifications);
+const ClipboardService = createExpoClipboardService(ExpoClipboard);
+const FileService = createExpoFileService({
+  fsModule: ExpoFS,
+  imagePickerModule: ExpoImagePicker,
+  mediaLibraryModule: ExpoMediaLibrary,
+  documentPickerModule: ExpoDocumentPicker,
+});
+
+const Stack = createStackNavigator();
+
+function messageStack() {
+    const { currentUser } = useSendbirdChat();
+
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+
+                
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
