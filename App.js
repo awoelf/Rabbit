dda7d00ac86120b46aefba63abedc5a7c1705866
rogseo <rogseo@gmail.com>
2023-01-26 08:39:39 -0600

    add sendbirdUIkit

--- App.js
+++ App.js
@@ -18,7 +18,41 @@ import { Provider } from 'react-redux';
 import store from './src/utils/store';
 
 // Theme
-import { rabbitTheme } from './src/styles/themes';
+import { rabbitMessageTheme, rabbitTheme } from './src/styles/themes';
+
+//sendbird
+import {
+  createNativeClipboardService,
+  createNativeFileService,
+  createNativeMediaService,
+  createNativeNotificationService,
+} from '@sendbird/uikit-react-native';
+
+import * as ExpoClipboard from 'expo-clipboard';
+import * as ExpoDocumentPicker from 'expo-document-picker';
+import * as ExpoFS from 'expo-file-system';
+import * as ExpoImagePicker from 'expo-image-picker';
+import * as ExpoMediaLibrary from 'expo-media-library';
+import * as ExpoNotifications from 'expo-notifications';
+
+import {
+  createExpoClipboardService,
+  createExpoFileService,
+  createExpoNotificationService,
+  SendbirdUIKitContainer,
+  useSendbirdChat,
+} from '@sendbird/uikit-react-native';
+
+
+const NotificationService = createExpoNotificationService(ExpoNotifications);
+const ClipboardService = createExpoClipboardService(ExpoClipboard);
+const FileService = createExpoFileService({
+  fsModule: ExpoFS,
+  imagePickerModule: ExpoImagePicker,
+  mediaLibraryModule: ExpoMediaLibrary,
+  documentPickerModule: ExpoDocumentPicker,
+});
+
 
 const httpLink = createHttpLink({
   uri: 'https://rabbit-app.herokuapp.com/graphql',
@@ -56,11 +90,30 @@ export default function App() {
 
   return (
     <ApolloProvider client={client}>
-      <Provider store={store}>
-        <NavigationContainer theme={rabbitTheme}>
-          <Main />
-        </NavigationContainer>
-      </Provider>
-    </ApolloProvider>
+      <SendbirdUIKitContainer
+        appId={'6CD12A00-3AA4-4F84-A4CB-C202BA86B06A'}
+        chatOptions={{ localCacheStorage: AsyncStorage }}
+        userProfile={{
+          onCreateChannel: (channel) => {
+            if (channel.isGroupChannel()) {
+              navigationActions.push(Routes.GroupChannel, { channelUrl: channel.url });
+            }
+          },
+        }}
+        platformServices={{
+          file: FileService,
+          notification: NotificationService,
+          clipboard: ClipboardService,
+        }}
+        styles={{ theme: rabbitMessageTheme }}
+      >
+        {/* <AppContext.Provider value={initialState}> */}
+
+          <NavigationContainer theme={rabbitTheme}>
+            <Main />
+          </NavigationContainer>
+        {/* </AppContext.Provider> */}
+      </SendbirdUIKitContainer>
+    </ApolloProvider >
   );
 }
