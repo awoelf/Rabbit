25e369c545b020dfeefadacc521655816a60966e
awoelf <98348782+awoelf@users.noreply.github.com>
2023-03-06 21:37:27 -0600

    added custom colors to message theme

--- src/styles/themes.js
+++ src/styles/themes.js
@@ -1,5 +1,5 @@
-import { createTheme, LightUIKitTheme } from '@sendbird/uikit-react-native-foundation';
-import { rabbit } from './palette';
+import { createTheme } from '@sendbird/uikit-react-native-foundation';
+import { rabbit, rabbitMessagePalette } from './palette';
 
 // Theme used by the navigation container
 export const rabbitTheme = {
@@ -9,33 +9,31 @@ export const rabbitTheme = {
     border: rabbit.border_color,
   },
 };
-
 // Theme used by the sendbird fragments
 export const rabbitMessageTheme = createTheme({
   colorScheme: 'rabbit',
-  colors: () => ({
-    ...LightUIKitTheme.colors,
-    primary: rabbit.accent_color,
+  palette: rabbitMessagePalette,
+  colors: (palette) => ({
+    ...palette.colors,
+    primary: 'black',
+    secondary: 'black',
     background: rabbit.background_color,
+    text: 'black',
     ui: {
-      ...LightUIKitTheme.colors.ui,
-      groupChannelPreview: {
-        default: {
-          none: {
-            background: rabbit.background_color
-          }
+      ...palette.colors.ui,
+      text: {
+        none: {
+          content: 'black',
+          background: 'black'
         }
-      }
-    },
-    
+      },
+    }
   }),
   typography: {
     shared: {
-      // fontFamily: rabbit.font_shared,
-      // fontSize: rabbit.font_size,
-    },
-    h1: {
-      fontFamily: rabbit.font_header,
-    },
+      fontFamily: rabbit.font_shared,
+      fontSize: rabbit.font_size,
+      lineHeight: 24,
+    }
   },
 });
