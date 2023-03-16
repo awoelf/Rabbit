ef22667b250751b5344138565156ec88798ed65d
Alexis Woelffer <98348782+awoelf@users.noreply.github.com>
2023-03-15 23:30:27 -0500

    added dialog response for log in and sign up and added privacy policy

--- src/screens/User/Login.js
+++ src/screens/User/Login.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Button, View, Text, TouchableOpacity, TextField, Icon } from 'react-native-ui-lib';
+import { Button, View, Text, TouchableOpacity, TextField, Icon, Dialog, Card } from 'react-native-ui-lib';
 import { useMutation } from '@apollo/client';
 import { LOGIN } from '../../utils/mutations';
 import decode from 'jwt-decode';
@@ -8,12 +8,14 @@ import auth from '../../utils/auth';
 import { useUserContext } from '../../utils/UserContext';
 
 // Styles and assets
-import { styles } from '../../styles/styles';
+import { styles, cardStyle } from '../../styles/styles';
 
 export default function Login(props) {
   const [email, setEmail] = useState('yeon@me.com');
   const [password, setPassword] = useState('password12345');
   const [login, { error, data }] = useMutation(LOGIN);
+  const [showFail, setShowFail] = useState(false);
+
   const userContext = useUserContext();
 
   const { connect } = useConnection();
@@ -36,9 +38,9 @@ export default function Login(props) {
       connect(mutationResponse.data.login.user.firstName, {
         nickname: mutationResponse.data.login.user.lastName,
       });
-      // props.navigation.navigate('Main');
     } catch (e) {
-      console.log(e, 'error here');
+      console.log(e, 'Unable to log user in.');
+      setShowFail(true);
     }
   };
 
@@ -48,7 +50,7 @@ export default function Login(props) {
         <Text style={styles.header1}>Rabbit</Text>
         <Icon source={require('../../assets/icon.png')} size={35} />
       </View>
-      <View marginH-30 flex-1>
+      <View marginH-30 flex-2>
         <Text style={styles.text} center>
           Log in to your Rabbit account
         </Text>
@@ -70,8 +72,7 @@ export default function Login(props) {
           id={'pwd'}
           onChangeText={setPassword}
         />
-      </View>
-      <View center marginT-s2>
+        <View centerH>
         <Button
           disabled={!email || !password}
           style={styles.button}
@@ -83,12 +84,29 @@ export default function Login(props) {
           <Text style={styles.text}>Log in</Text>
         </Button>
       </View>
-      <View flex-3 centerH bottom>
+      </View>
+      
+      <View flex-1 centerH bottom>
         <Text style={styles.text}>Don't have an account?</Text>
         <TouchableOpacity onPress={() => props.navigation.navigate('Register')}>
           <Text style={styles.link}>Sign Up</Text>
         </TouchableOpacity>
       </View>
+      
+      <Dialog
+        visible={showFail}
+        onDismiss={() => {
+          setShowFail(false);
+        }}
+      >
+        <Card style={cardStyle}>
+          <View margin-s4>
+            <Text style={styles.text} center>
+              Incorrect email or password. Please try again.
+            </Text>
+          </View>
+        </Card>
+      </Dialog>
     </View>
   );
 }
